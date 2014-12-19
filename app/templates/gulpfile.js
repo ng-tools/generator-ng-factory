feafb71ad9805bce2ec378e9df918e65c961e1bc
Olivier Louvignes <olivier@mg-crea.com>
2014-12-19 15:43:04 +0100

    feat(gulp): add gulp aliases

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,4 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
-require('ng-factory').use(gulp);
+var config = require('ng-factory').use(gulp);
+
+//
+// Aliases
+
+gulp.task('serve', ['ng:serve']);
+gulp.task('build', ['ng:build']);
+gulp.task('test', ['ng:test']);
